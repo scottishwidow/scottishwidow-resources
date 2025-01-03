cce66b54656cebf863b18a312008dbc071c714f7
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-03 17:56:18 +0200

    subnets added

---
+++ modules/vpc/subnets.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "private_zone-1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr_block
+  availability_zone = var.az-1
+}
+
+resource "aws_subnet" "public_zone-1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr_block
+  availability_zone = var.az-2
+  map_public_ip_on_launch = true
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,10 +3,34 @@ variable "tags" {
   type = map(any)
 }
 
-variable "cidr_block" {
+variable "vpc_cidr_block" {
   description = "CIDR Block for VPC"
   type = string
   default = "172.16.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR Block for Public Subnet"
+  type = string
+  default = "172.16.0.0/19"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR Block for Private Subnet"
+  type = string
+  default = "172.16.32.0/19"
+}
+
+variable "az-1" {
+  type = string
+  description = "Private AZ"
+  default = "eu-west-1a"
+}
+
+variable "az-2" {
+  type = string
+  description = "Public AZ"
+  default = "eu-west-1b"
 }
 
 variable "region" {
