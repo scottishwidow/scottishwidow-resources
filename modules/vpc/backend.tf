e30d9f7abc7210b6ab89ac04383d1a036e725ab1
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-03 18:22:46 +0200

    remote backend reconfigured

--- modules/vpc/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = var.terraform_state_bucket_name
-    key = "backend.tfstate"
-    region = var.region
-    dynamodb_table = var.terraform_state_lock
-    encrypt = true
-  }
-}
