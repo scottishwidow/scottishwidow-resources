e778f78bb22bc9b5062b77839a9be74bdb6181fd
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-03 15:52:31 +0200

    main.tf init

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -1,3 +1,44 @@
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "tf-state-${local.environment_prefix}"
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "terraform_state_bucket_ownership" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  rule {
+    object_ownership = "BucketOwneerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "terraform_state_bucket_acl" {
+  depends_on = [ aws_s3_bucket_ownership_controls.terraform_state_bucket_ownership ]
+
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = "tf-state-lock"
+  billing_mode = "PROVISIONED" # for predictable workloads
+
+  hash_key = "LockID" # primary key attribute used to uniquely identify items in the table
+
+  attribute { # Defines the schema for the primary key attributes in the table
+    name = "LockID"
+    type = "S" # S for string
+  }
+
+  read_capacity = 1
+  write_capacity = 1
+
+  tags = var.tags
 }
