69185416a1e23c43d6aab0c6155d6cc7c710b3c6
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-03 18:26:33 +0200

    backend placed into main

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,3 +2,13 @@ module "vpc" {
   source = "../modules/vpc"
   tags = { env = "test-assignment", management = "terraform" } 
 }
+
+terraform {
+  backend "s3" {
+    bucket = ""
+    key = "backend.tfstate"
+    region = "eu-west-1"
+    dynamodb_table = ""
+    encrypt = true
+  }
+}
--- vpc/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = ""
-    key = "backend.tfstate"
-    region = ""
-    dynamodb_table = ""
-    encrypt = true
-  }
-}
