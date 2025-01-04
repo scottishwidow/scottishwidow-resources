31743e21fa17a5543ae9b0da0fdf40648b49f62e
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-04 21:13:19 +0200

    rds fixes

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,4 +1,4 @@
-resource "aws_db_subnet_group" "rds_subnet_group" {
+resource "aws_db_subnet_group" "rds_subnet_group" { # todo add one more private subnet for HA 
   name = "rds-subnet-group"
   subnet_ids = [ data.aws_subnet.selected.id ]
 }
@@ -17,18 +17,18 @@ resource "aws_security_group" "rds_security_group" {
 }
 
 resource "aws_db_instance" "rds_instance" {
-  engine = "mysql"
-  engine_version = var.mysql_engine_version
-  skip_final_snapshot = true
+  engine                  = "mysql"
+  engine_version          = var.mysql_engine_version
+  skip_final_snapshot     = true
   final_snapshot_identifier = var.final_snapshot_identifier
-  instance_class = "db.t2.micro"
-  allocated_storage = 20
-  identifier = var.instance_identifier
-  db_name = var.db_name
-  username = var.db_username
-  password = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = [ aws_secutiry_group.rds_security_group.id ]
+  instance_class          = "db.t2.micro"
+  allocated_storage       = 20
+  identifier              = var.instance_identifier
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_security_group.id]
 
   tags = var.tags
 }
