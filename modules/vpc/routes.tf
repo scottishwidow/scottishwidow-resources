09522c9e551198cbdf91962a54336e8e138079c9
scottishwidow <mike.voznyanskix@gmail.com>
2025-05-25 22:27:05 +0300

    wordpress specific removed

--- modules/vpc/routes.tf
+++ modules/vpc/routes.tf
@@ -2,17 +2,17 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Public = false
+    availability = public
   }
 }
 
 resource "aws_route" "public_route" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public_zone-1" {
-  subnet_id = aws_subnet.public_zone-1.id
+  subnet_id      = aws_subnet.public_zone-1.id
   route_table_id = aws_route_table.public.id
 }
