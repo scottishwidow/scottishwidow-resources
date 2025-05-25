14aa18d25fa13b7e54599e23e4892b7d570d5559
scottishwidow <mike.voznyanskix@gmail.com>
2025-05-25 18:12:58 +0300

    fix: bootstrap version fixed

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,4 +1,4 @@
 module "bootstrap" {
   source = "../modules/bootstrap"
-  tags = { env = "test-assignment", management = "terraform" }
+  tags   = { env = "test-assignment", management = "terraform" }
 }
