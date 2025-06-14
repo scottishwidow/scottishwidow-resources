ae54b2b8e27995e2e81bc54a4ec8fd7d4284fd9a
scottishwidow <mike.voznyanskix@gmail.com>
2025-06-14 22:19:47 +0300

    rds: ephemeral values

--- development/main.tf
+++ development/main.tf
@@ -3,3 +3,11 @@ module "vpc" {
   tags   = { environment = "development", management = "terraform" }
   region = data.aws_region.current.name
 }
+
+module "database" {
+  source = "../modules/database"
+  postgres_instance_identifier = "scottishwidow-postgres"
+  subnet_ids = [ module.vpc.public_zone-1, module.vpc.private_zone-1 ]
+  tags = { environment = "development", management = "terraform" }
+  db_password_secret_name = "postgres_pwd_secret"
+}
