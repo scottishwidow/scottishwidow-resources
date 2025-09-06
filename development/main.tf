40298d9a27f35b52b16f3493af43e77e45237b05
scottishwidow <mike.voznyanskix@gmail.com>
2025-09-06 12:13:03 +0300

    chore(rds): count applied to all resources

--- development/main.tf
+++ development/main.tf
@@ -10,7 +10,6 @@ module "database" {
   subnet_ids = [ module.vpc.public_zone-1, module.vpc.private_zone-1 ]
   tags = { environment = "development", management = "terraform" }
   db_password_secret_name = "postgres_pwd_secret"
-  create_postgres = true
   postgres_sg_name = "rds_postgres_sg"
   vpc_id = module.vpc.vpc_id
 }
