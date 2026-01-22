063488d5f1f7d3557db386e23994665a889df7bd
scottishwidow <mike.voznyanskix@gmail.com>
2026-01-22 22:26:16 +0000

    chore: adjust state prefix

--- live/management/backend.tf
+++ live/management/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-state-scottishwidow-management"
-    key          = "dev/terraform.tfstate"
+    key          = "mgmt/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
     use_lockfile = true
