6575a61da59623d24b7f5c81172237d81e0e9f77
scottishwidow <mike.voznyanskix@gmail.com>
2026-01-22 22:24:40 +0000

    chore: adjust module paths

--- live/management/main.tf
+++ live/management/main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source = "../modules/vpc"
+  source = "../../modules/vpc"
   tags   = { environment = "management", management = "terraform", Name = "scottishwidow" }
   region = var.aws_region
 }
 
 module "alb" {
-  source = "../modules/alb"
+  source = "../../modules/alb"
   tags = { environment = "management", management = "terraform" }
   vpc_id = module.vpc.vpc_id
   alb_subnets = [module.vpc.public_zone_1, module.vpc.public_zone_2]
@@ -14,7 +14,7 @@ module "alb" {
 }
 
 module "database" {
-  source = "../modules/database"
+  source = "../../modules/database"
   postgres_instance_identifier = "scottishwidow-postgres"
   subnet_ids = [ module.vpc.public_zone_1, module.vpc.private_zone_1 ]
   tags = { environment = "management", management = "terraform" }
