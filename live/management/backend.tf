bc4ef8cd695f56bea384951f7b173b4b9d0ab561
scottishwidow <mike.voznyanskix@gmail.com>
2026-01-22 22:23:21 +0000

    chore: configure backend

--- live/management/backend.tf
+++ live/management/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tf-state-scottishwidow-mgmt"
+    bucket       = "tf-state-scottishwidow-management"
     key          = "dev/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
