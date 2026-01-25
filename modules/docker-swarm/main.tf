c814ee448c080c54b0a6c5e7ac8d6cbd9822fedc
scottishwidow <mike.voznyanskix@gmail.com>
2026-01-25 14:14:31 +0000

    chore: define SG rules for swarm as separate resources

--- modules/docker-swarm/main.tf
+++ modules/docker-swarm/main.tf
@@ -3,54 +3,66 @@ resource "aws_security_group" "swarm" {
   description = "Security group for Docker Swarm nodes"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_blocks
-  }
-
-  ingress {
-    description = "Swarm manager"
-    from_port   = 2377
-    to_port     = 2377
-    protocol    = "tcp"
-    self        = true
-  }
-
-  ingress {
-    description = "Swarm node communication"
-    from_port   = 7946
-    to_port     = 7946
-    protocol    = "tcp"
-    self        = true
-  }
-
-  ingress {
-    description = "Swarm node communication (UDP)"
-    from_port   = 7946
-    to_port     = 7946
-    protocol    = "udp"
-    self        = true
-  }
-
-  ingress {
-    description = "Overlay network"
-    from_port   = 4789
-    to_port     = 4789
-    protocol    = "udp"
-    self        = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = var.tags
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  security_group_id = aws_security_group.swarm.id
+  description       = "SSH"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.ssh_cidr_blocks
+}
+
+resource "aws_security_group_rule" "swarm_manager" {
+  type              = "ingress"
+  security_group_id = aws_security_group.swarm.id
+  description       = "Swarm manager"
+  from_port         = 2377
+  to_port           = 2377
+  protocol          = "tcp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "swarm_node_tcp" {
+  type              = "ingress"
+  security_group_id = aws_security_group.swarm.id
+  description       = "Swarm node communication"
+  from_port         = 7946
+  to_port           = 7946
+  protocol          = "tcp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "swarm_node_udp" {
+  type              = "ingress"
+  security_group_id = aws_security_group.swarm.id
+  description       = "Swarm node communication (UDP)"
+  from_port         = 7946
+  to_port           = 7946
+  protocol          = "udp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "overlay_network" {
+  type              = "ingress"
+  security_group_id = aws_security_group.swarm.id
+  description       = "Overlay network"
+  from_port         = 4789
+  to_port           = 4789
+  protocol          = "udp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "all_egress" {
+  type              = "egress"
+  security_group_id = aws_security_group.swarm.id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "node" {
