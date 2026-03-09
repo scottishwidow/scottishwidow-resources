34511ce915a9a3a18a427d30e10983558c258315
scottishwidow <mike.voznyanskix@gmail.com>
2026-03-09 17:49:01 +0000

    feat: add VPC & pre-commit config

---
+++ live/development/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.0"
+
+  name = "development"
+  cidr = "10.0.0.0/16"
+
+  map_public_ip_on_launch = true
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    management = "terraform"
+    environment = "development"
+  }
+}
