a3df431d3bcdf4c911ec05e28930bd4ce8411aaa
Michael Korotchenko <135420876+scottishwidow@users.noreply.github.com>
2026-05-28 20:38:28 +0300

    chore: clean up after accounts retirement (#14)

--- live/development/bootstrap/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "terraform_state_bucket_name" {
-  value = module.bootstrap.terraform_state_bucket_name
-}
--- live/management/variables.tf
+++ live/management/variables.tf
@@ -10,20 +10,6 @@ variable "aws_region" {
 
 variable "env" {
   description = "Deployment environment (dev, stage, prod)"
-  type = string
-  default = "management"
-}
-
-#########################
-# Docker Swarm
-#########################
-
-variable "key_name" {
-  description = "Name of the SSH Key"
   type        = string
-}
-
-variable "docker_swarm_instance_type" {
-  description = "Instance type for Docker Swarm nodes"
-  type        = string
+  default     = "management"
 }
